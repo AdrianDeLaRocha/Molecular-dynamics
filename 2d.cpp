8ac9e33a5425877d6d043418f69c42f90d1e1e03
AdrianDeLaRocha <48077677+AdrianDeLaRocha@users.noreply.github.com>
2019-04-06 13:59:05 -0700

    Add files via upload

---
+++ 2d.cpp
@@ -0,0 +1,201 @@
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+using namespace std;
+
+#define STEP 100
+#define Num_x 100
+#define Num_y 100
+#define TT 5.0
+#define M 1.0
+#define K 500.0
+#define D 5.0
+#define DT 0.0001
+
+struct particle{
+    double x;
+    double y;
+    double vel_x;
+    double vel_y;
+    double acc_x;
+    double acc_y;
+    double For_x;
+    double For_y;
+    double Pot_x;
+    double Pot_y;
+    int id;
+};
+
+double* mol_dyn(int Num_tot, struct particle data[]){
+    int i, n, Num;
+    double *s[Num_tot], *v[Num_tot], *a[Num_tot], *F[Num_tot];
+
+    // Getting updates for the x-axis.
+    n = 1; // Defining closest neighbors according to numeration and axis.
+    Num = Num_x;
+    for(i = 0; i < (Num_tot); i++){
+        *s[i] = data[i].x;
+        *v[i] = data[i].vel_x;
+        *a[i] = data[i].acc_x;
+        *F[i] = data[i].For_x;
+    }
+    for(i = 0; i < (Num_tot); i++){
+        if((i + Num) % Num == 0){
+            *F[i] = (-K * (*s[i] - D)) + (-K * (*s[i] - *s[i+n] + D));
+        }
+        else if((i + Num) % Num == (Num - 1)){
+            *F[i] = (-K * (*s[i] - *s[i-n] - D)) + (-K * (*s[i] - (Num * D)));
+        }
+        else{
+            *F[i] = (-K * (*s[i] - *s[i-n] - D)) + (-K * (*s[i] - *s[i+n] + D));
+        }
+        *a[i] = *F[i] / M;
+        *v[i] = *v[i] + (*a[i] * DT);
+        *s[i] = *s[i] + (*v[i] * DT) + (*a[i] * 0.5 * pow(DT, 2.0));
+    }
+
+    // Getting updates for the y-axis.
+    n = Num_x; // Defining closest neighbors according to numeration and axis.
+    Num = Num_y;
+    for(i = 0; i < (Num_tot); i++){
+        *s[i] = data[i].y;
+        *v[i] = data[i].vel_y;
+        *a[i] = data[i].acc_y;
+        *F[i] = data[i].For_y;
+    }
+    for(i = 0; i < (Num_tot); i++){
+        if(i < Num_x){
+            *F[i] = (-K * (*s[i] - D)) + (-K * (*s[i] - *s[i+n] + D));
+        }
+        else if(((i - 1) / Num_x == Num_y)){
+            *F[i] = (-K * (*s[i] - *s[i-n] - D)) + (-K * (*s[i] - (Num * D)));
+        }
+        else{
+            *F[i] = (-K * (*s[i] - *s[i-n] - D)) + (-K * (*s[i] - *s[i+n] + D));
+        }
+        *a[i] = *F[i] / M;
+        *v[i] = *v[i] + (*a[i] * DT);
+        *s[i] = *s[i] + (*v[i] * DT) + (*a[i] * 0.5 * pow(DT, 2.0));
+    }
+}
+
+double* pos_arr(int Num_tot, struct particle data[]){
+    char c, axis;
+    int i;
+
+    for(i = 0; i < Num_tot; i++){
+        data[i].x = 0.0;
+        data[i].y = 0.0;
+    }
+
+    printf("Define initial positions? [y/n] ");
+    scanf(" %c", &c);
+    while(c != 'y' && c != 'n'){
+        printf("Invalid. [y/n] ");
+        scanf(" %c", &c);
+    }
+
+    while(c == 'y'){
+        printf("Axis: ");
+        scanf(" %c", &axis);
+        printf("i: ");
+        scanf(" %d", &i);
+        printf("%c[%d]: ", axis, i);
+        if(axis == 'x'){
+            scanf(" %lf", &data[i].x);
+        }
+        if(axis == 'y'){
+            scanf(" %lf", &data[i].y);
+        }
+        printf("Add another initial position? [y/n] ");
+        scanf(" %c", &c);
+        while(c != 'y' && c != 'n'){
+            printf("Invalid. [y/n] ");
+            scanf(" %c", &c);
+        }
+    }
+}
+
+double* vel_arr(int Num_tot, struct particle data[]){
+    char c, axis;
+    int i;
+
+    for(i = 0; i < Num_tot; i++){
+        data[i].vel_x = 0.0;
+        data[i].vel_y = 0.0;
+    }
+
+    printf("Define initial velocities? [y/n] ");
+    scanf(" %c", &c);
+    while(c != 'y' && c != 'n'){
+        printf("Invalid. [y/n] ");
+        scanf(" %c", &c);
+    }
+
+    while(c == 'y'){
+        printf("Axis: ");
+        scanf(" %c", &axis);
+        printf("i: ");
+        scanf(" %d", &i);
+        printf("v%c[%d]: ", axis, i);
+        if(axis == 'x'){
+            scanf(" %lf", &data[i].vel_x);
+        }
+        if(axis == 'y'){
+            scanf(" %lf", &data[i].vel_y);
+        }
+        printf("Add another initial velocity? [y/n] ");
+        scanf(" %c", &c);
+        while(c != 'y' && c != 'n'){
+            printf("Invalid. [y/n] ");
+            scanf(" %c", &c);
+        }
+    }
+}
+
+int main(){
+    ofstream myfile;
+    myfile.open ("data.csv");
+    int Num_tot = Num_x * Num_y;
+    int i, id[Num_tot], print_ctrl = STEP;
+    double t;
+    struct particle data[Num_tot];
+    std::cout << std::fixed << std::setprecision(5);
+    vel_arr(Num_tot, data);
+    pos_arr(Num_tot, data);
+
+    for(i = 0; i < Num_tot; i++){
+        data[i].x += (((i + 1) % Num_x) * D);
+        data[i].y += (((i / Num_x) + 1) * D);
+        data[i].acc_x = 0.0;
+        data[i].acc_y = 0.0;
+        data[i].For_x = 0.0;
+        data[i].For_y = 0.0;
+    }
+
+    for(i = 0; i < Num_tot; i++){
+        myfile << "x" << i << ", ";
+    }
+    for(i = 0; i < Num_tot; i++){
+        myfile << "y" << i << ", ";
+    }
+    myfile << "\n";
+
+    for(t = 0.0; t <= TT; t += DT){
+        mol_dyn(Num_tot, data);
+        if((print_ctrl % STEP) == 0){
+            myfile << t << ", ";
+            for(i = 0; i < Num_tot; i++){
+                myfile << data[i].x << ", ";
+            }
+            for(i = 0; i < Num_tot; i++){
+                myfile << data[i].y << ", ";
+            }
+            myfile << "\n";
+        }
+        print_ctrl++;
+    }
+myfile.close();
+}
